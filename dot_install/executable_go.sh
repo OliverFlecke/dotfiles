6d648245fedb4cb4e07bb8e00c7d50341c0e1215
Oliver Fleckenstein <oliverfl@live.dk>
2024-01-11 13:05:01 +0800

    chore: Migrated rtx to mise

---
+++ dot_install/executable_go.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+go install golang.org/x/tools/gopls@latest          # LSP
+go install github.com/go-delve/delve/cmd/dlv@latest # Debugger
+go install golang.org/x/tools/cmd/goimports@latest  # Formatter
