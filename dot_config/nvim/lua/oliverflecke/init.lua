139925ed9d7a81e7efc14d8b01cf499ce80b11a8
Oliver Fleckenstein <oliverfl@live.dk>
2024-12-13 19:46:48 +0800

    config: nvim

---
+++ dot_config/nvim/lua/oliverflecke/init.lua
@@ -0,0 +1,2 @@
+require("oliverflecke.remap")
+require("oliverflecke.set")
---
+++ dot_config/nvim/lua/oliverflecke/remap.lua
@@ -0,0 +1,23 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Move lines up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Yank to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Deleting to void register
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
