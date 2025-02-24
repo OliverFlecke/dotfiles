a2a5275f81d993e540a70e39c519d54f89879ff7
Oliver Fleckenstein <oliverfl@live.dk>
2025-02-24 09:48:27 +0800

    config: removed unused nvim config files

--- dot_config/nvim/lua/lsp.lua
+++
@@ -1,8 +0,0 @@
--- Configuration for lsp
-
-local lspconfig = require('lspconfig')
-local servers = {
-	"pyright",
-}
-
-lspconfig.pyright.setup({})
--- dot_config/nvim/lua/nerdtree.lua
+++
@@ -1,4 +0,0 @@
-
-require 'paq' {
- 'preservim/nerdtree';
-}
