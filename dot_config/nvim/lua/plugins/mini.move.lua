df71dce5b0091f05012069acbba2c807d52d21be
Oliver Fleckenstein <oliverfl@live.dk>
2025-02-24 09:47:04 +0800

    config: neovim updates

--- dot_config/nvim/lua/plugins/mini.move.lua
+++ dot_config/nvim/lua/plugins/mini.move.lua
@@ -1,7 +1,27 @@
 return {
 	'echasnovski/mini.move',
-	version = false,
+	version = '*',
 	config = function()
-		require("mini.move").setup()
-	end
+		require("mini.move").setup({
+			mappings = {
+				-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+				left = '<M-h>',
+				right = '<M-l>',
+				down = '<M-j>',
+				up = '<M-k>',
+
+				-- Move current line in Normal mode
+				line_left = '<M-h>',
+				line_right = '<M-l>',
+				line_down = '<M-j>',
+				line_up = '<M-k>',
+			},
+
+			-- Options which control moving behavior
+			options = {
+				-- Automatically reindent selection during linewise vertical move
+				reindent_linewise = true,
+			},
+		})
+	end,
 }
--- dot_config/nvim/lua/plugins/vim-fugitive.lua
+++ dot_config/nvim/lua/plugins/vim-fugitive.lua
@@ -1,3 +1,7 @@
 return {
-	"tpope/vim-fugitive"
+	"tpope/vim-fugitive",
+	enabled = false,
+	config = function()
+		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+	end,
 }
