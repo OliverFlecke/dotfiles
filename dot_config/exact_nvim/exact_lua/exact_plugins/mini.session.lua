c7574e1e982cae6df72fc06c63ded0253fb2c4d9
Oliver Fleckenstein <oliverfl@live.dk>
2025-04-05 12:54:21 +0800

    config: nvim dashboard + remove mini.session

--- dot_config/exact_nvim/exact_lua/exact_plugins/mini.session.lua
+++
@@ -1,9 +0,0 @@
-return {
-	'echasnovski/mini.sessions',
-	version = false,
-	enabled = false,
-	opts = {
-		autoread = false,
-		file = 'session.vim',
-	},
-}
--- dot_config/exact_nvim/exact_lua/exact_plugins/snacks.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/snacks.lua
@@ -1,14 +1,24 @@
 return {
 	"folke/snacks.nvim",
-	-- keys = {
-	-- 	{
-	-- 		"<leader>lg",
-	-- 		function()
-	-- 			Snacks.lazygit.open()
-	-- 		end
-	-- 	},
-	-- },
+	lazy = false,
+	priority = 1000,
+	keys = {
+		-- git
+		{ "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
+		{ "<leader>gl", function() Snacks.picker.git_log() end,      desc = "Git Log" },
+		{ "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
+		{ "<leader>gs", function() Snacks.picker.git_status() end,   desc = "Git Status" },
+		{ "<leader>gS", function() Snacks.picker.git_stash() end,    desc = "Git Stash" },
+		{ "<leader>gd", function() Snacks.picker.git_diff() end,     desc = "Git Diff (Hunks)" },
+		{ "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+		{ "<leader>lg", function() Snacks.lazygit.open() end,        desc = "Open LazyGit" },
+
+		{ "<leader>f",  function() Snacks.picker.smart() end,        desc = "Smart Find Files" },
+		{ "<leader>,",  function() Snacks.picker.buffers() end,      desc = "Buffers" },
+		{ "<leader>/",  function() Snacks.picker.grep() end,         desc = "Grep" },
+	},
 	opts = {
+		input = {},
 		animate = {},
 		picker = {},
 		lazygit = {},
@@ -16,18 +26,17 @@ return {
 		dashboard = {
 			sections = {
 				{ section = "header" },
-				{ section = "keys", gap = 1, padding = 1 },
-				{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = { 2, 2 } },
-				{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
+				{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+				{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+				{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
 				{
 					pane = 2,
 					icon = " ",
 					desc = "Browse Repo",
 					padding = 1,
 					key = "b",
-					action = function()
-						Snacks.gitbrowse()
-					end,
+					enabled = function() return Snacks.git.get_root() ~= nil end,
+					action = function() Snacks.gitbrowse() end,
 				},
 				function()
 					local in_git = Snacks.git.get_root() ~= nil
