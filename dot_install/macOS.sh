cd9844684f4d77144845050e52ba186047fb053c
Oliver Fleckenstein <oliverfl@live.dk>
2025-05-17 22:56:29 +0800

    config: additions to install tools

--- dot_install/macOS.sh
+++ dot_install/macOS.sh
@@ -7,3 +7,5 @@ $dir/macOS/defaults.me.sh
 $dir/macOS/defaults.dock.sh
 $dir/macOS/fonts.sh
 $dir/macOS/get.sh
+
+brew install --cask nikitabobko/tap/aerospace
--- dot_install/macOS/executable_fonts.sh
+++ dot_install/macOS/executable_fonts.sh
@@ -1,3 +1,6 @@
 brew tap homebrew/cask-fonts
 brew install --cask font-fira-mono-for-powerline
 brew install --cask font-meslo-lg-nerd-font
+
+brew tap mbwilding/neospleen
+brew install --cask font-neospleen-nerd-font
